bc0ee6d276fc0f18ebf33c8537dba71d837ba3c7
jinyong <jinyong0826@gmail.com>
2023-08-05 15:04:10 +0900

    init commit

---
+++ src/main/kotlin/fi400/glass_bottle/GlassBottleApplication.kt
@@ -0,0 +1,11 @@
+package fi400.glass_bottle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GlassBottleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GlassBottleApplication>(*args)
+}
---
+++ src/test/kotlin/fi400/glass_bottle/GlassBottleApplicationTests.kt
@@ -0,0 +1,14 @@
+package fi400.glass_bottle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GlassBottleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        println("Hello World!")
+    }
+
+}
