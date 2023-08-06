163db5a5c1d6f7fc792e56af0ba9dbd8b5147694
jinyong <jinyong0826@gmail.com>
2023-08-06 15:03:44 +0900

    유틸리티 개발 (Message, Logger)

---
+++ src/main/kotlin/fi400/glass_bottle/commons/utils/Message.kt
@@ -0,0 +1,33 @@
+package fi400.glass_bottle.commons.utils
+
+/**
+ * http request & response 처리용 유틸리티
+ * Builder 패턴으로 작성됨
+ * @author jinyong
+ */
+
+/**
+ * @sample
+ *      return Message.Builder()
+ *             .status(HttpStatus.OK.value())
+ *             .message("Success")
+ *             .data(item)
+ *             .build()
+ */
+data class Message(
+    val status: Int,
+    val message: String,
+    val data: Any? = null
+) {
+    class Builder {
+        private var status: Int = 0
+        private var message: String = ""
+        private var data: Any? = null
+
+        fun status(status: Int) = apply { this.status = status }
+        fun message(message: String) = apply { this.message = message }
+        fun data(data: Any?) = apply { this.data = data }
+
+        fun build() = Message(status, message, data)
+    }
+}
