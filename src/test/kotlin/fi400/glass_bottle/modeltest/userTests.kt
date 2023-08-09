645357c3165176c5e17c636112995b59731a08c3
jinyong <jinyong0826@gmail.com>
2023-08-09 21:42:05 +0900

    feat kakao 토큰 클래스 개발
    KakaoTokens.kt : accessToken 관련 데이터를 저장하는 클래스

---
+++ src/test/kotlin/fi400/glass_bottle/modeltest/userTests.kt
@@ -0,0 +1,14 @@
+package fi400.glass_bottle.modeltest
+
+import fi400.glass_bottle.model.entity.User
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class userTests {
+
+    // 유저 테스트 작성
+
+    // 편지 테스트 작성
+}
